fe7af17fc172e2dd38e003c447258c5b3d515776
Egor Soluynov <167007583+TechStrelka@users.noreply.github.com>
2026-03-28 22:39:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -55,9 +55,8 @@ unsigned int seqCollatz(unsigned int *maxlen,
 
         while (n > 1) {
             if (n % 2 == 0) {
-                n = n / 2; 
-            }
-            else {
+                n = n / 2;
+            } else {
                 n = 3 * n + 1;
             }
             current_len++;
